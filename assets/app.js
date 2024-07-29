d6514505e85ee1c6680a6e49b14e0e44dc540f6f
ScuBix1 <bastian.monnin1@gmail.com>
2024-07-29 23:01:34 +0200

    display infos of client dynamically

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,7 @@ import './bootstrap.js';
 import './styles/app.css';
 document.addEventListener("DOMContentLoaded", function(){
     if(location.href == "http://localhost:8000/"){
-        let clients = document.querySelector('.select_client');
+        let clients = document.querySelector('select_client');
         clients.forEach((client) => {
             console.log(client)
         });
